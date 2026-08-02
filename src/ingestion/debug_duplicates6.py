02c7714f8da5d9f1f4bc293ee18184fcc377f24c
DRAKismo23 <sasukesenpai48@gmail.com>
2026-08-02 04:58:10 +0100

    Remove throwaway debug scripts

--- src/ingestion/debug_duplicates6.py
+++
@@ -1,9 +0,0 @@
-with open("data/processed/code_penal_fr_2023.txt", encoding="utf-8") as f:
-    content = f.read()
-
-idx = content.find("DISPOSITIONS PRELIMINAIRES")
-trimmed = content[idx:]
-
-# Print from the start of the embedded "Article 331" section onward,
-# to find where it ends and real Code Pénal content resumes
-print(trimmed[110975:113000])
